c1806371c16f1206e9b506f9e3551adddb188e5b
zootechdrum <zootechdrum@gmail.com>
2019-01-22 21:18:39 -0800

    upload schema for mysql database.
    Gives you a good template to start a mysql database

---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users(
+    email VARCHAR(255) PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO users (email) VALUES
+('ZOOTECH@YAHOO.COM'),("JUNEDAY@GMAIL.COM");
